58c511b55c026537361889f7227b2c49531a42e3
Jannik Oeschger <oeschgerjannik@gmail.com>
2021-12-11 12:14:01 +0100

    ✨ Added routes
    ADdeds route so you can navigate to the about-page and back

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import {v4 as uuidv4} from 'uuid';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import AboutPage from "./pages/AboutPage";
+import AboutIconLink from "./components/AboutIconLink";
 
 function App() {
 
@@ -23,15 +26,24 @@ function App() {
     }
 
     return (
-        <>
+        <Router>
             <Header/>
             <div className='container'>
-                <FeedbackForm handleAdd={addFeedback}/>
-                <FeedbackStats feedback={feedback}/>
-                <FeedbackList feedback={feedback}
-                              handleDelete={deleteFeedback}/>
+                <Routes>
+                    <Route exact path={"/"} element={
+                        <>
+                            <FeedbackForm handleAdd={addFeedback}/>
+                            <FeedbackStats feedback={feedback}/>
+                            <FeedbackList feedback={feedback}
+                                          handleDelete={deleteFeedback}/>
+
+                        </>
+                    }/>
+                    <Route path='/about' element={<AboutPage/>}/>
+                </Routes>
+                <AboutIconLink/>
             </div>
-        </>
+        </Router>
     )
 }
 
