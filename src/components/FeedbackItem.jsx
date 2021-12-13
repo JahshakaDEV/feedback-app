008ef04357ac1a9ef1a29f6c99dada0dfeaf4d4b
Jannik Oeschger <oeschgerjannik@gmail.com>
2021-12-13 20:39:09 +0100

    ✨ You can now edit Feedbacks
    added button and function to edit feedbacks

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,18 +1,21 @@
-import {FaTimes} from 'react-icons/fa'
+import {FaTimes, FaEdit} from 'react-icons/fa'
 import React, {useContext} from 'react';
 import Card from "./shared/Card";
 import PropTypes from "prop-types";
 import FeedbackContext from "../context/FeedbackContext";
 
-function FeedbackItem({item, reverse}) {
+function FeedbackItem({item}) {
 
-    const {deleteFeedback} = useContext(FeedbackContext)
+    const {deleteFeedback, editFeedback, reverse} = useContext(FeedbackContext)
 
     return (
         <Card reverse={reverse}>
             <div className={"num-display"}>{item.rating}</div>
             <button className={"close"} onClick={() => deleteFeedback(item.id)}>
                 <FaTimes color={'purple'}/>
+            </button>
+            <button onClick={() => editFeedback(item)} className={"edit"}>
+                <FaEdit color={'purple'}/>
             </button>
             <div className={"text-display"}>{item.text}</div>
         </Card>
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -4,13 +4,31 @@ import {v4 as uuidv4} from "uuid";
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) => {
-    const [feedback, setFeedback] = useState([{
-        id: 1,
-        text: 'Item from context',
-        rating: 10
-    }])
-
+    const [feedback, setFeedback] = useState([
+        {
+            id: 1,
+            text: 'Item from context1',
+            rating: 10
+        }, {
+            id: 2,
+            text: 'Item from context2',
+            rating: 8
+        }, {
+            id: 3,
+            text: 'Item from context3',
+            rating: 3
+        }, {
+            id: 4,
+            text: 'Item from context4',
+            rating: 6
+        },
+    ])
+    const [feedbackEdit, setFeedbackEdit] = useState({item: {}, edit: false});
     const [reverse, setReverse] = useState(false);
+
+    const editFeedback = (item) => {
+        setFeedbackEdit({item, edit: true})
+    }
 
     const deleteFeedback = (id) => {
         if (window.confirm('Are you sure you want to delete?')) {
@@ -24,12 +42,20 @@ export const FeedbackProvider = ({children}) => {
         setFeedback([newFeedback, ...feedback])
     }
 
+    const updateFeedback = (id, updItem) => {
+        setFeedback(feedback.map((item) => (
+            item.id === id ? {...item, ...updItem} : item)))
+    }
+
     return <FeedbackContext.Provider value={{
         feedback,
         deleteFeedback,
         addFeedback,
         reverse,
-        setReverse
+        setReverse,
+        editFeedback,
+        feedbackEdit,
+        updateFeedback
     }}>
         {children}
     </FeedbackContext.Provider>
