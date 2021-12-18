868c44cfd70c725dffe8f474eded5c65d61dcaff
Jannik Oeschger <oeschgerjannik@gmail.com>
2021-12-18 16:05:53 +0100

    🗃️ Added mock database
    Added mock database from which the feedbacks are loaded now.

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,30 +1,25 @@
-import {createContext, useState} from "react";
+import {createContext, useState, useEffect} from "react";
 import {v4 as uuidv4} from "uuid";
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) => {
-    const [feedback, setFeedback] = useState([
-        {
-            id: 1,
-            text: 'Item from context1',
-            rating: 10
-        }, {
-            id: 2,
-            text: 'Item from context2',
-            rating: 8
-        }, {
-            id: 3,
-            text: 'Item from context3',
-            rating: 3
-        }, {
-            id: 4,
-            text: 'Item from context4',
-            rating: 6
-        },
-    ])
+    const [loading, setLoading] = useState(true);
+    const [feedback, setFeedback] = useState([])
     const [feedbackEdit, setFeedbackEdit] = useState({item: {}, edit: false});
     const [reverse, setReverse] = useState(false);
+
+    useEffect(() => {
+        fetchFeedback()
+    }, []);
+
+    const fetchFeedback = async () => {
+        const response = await fetch("http://localhost:5000/feedback?_sort=id&_order?desc")
+        const data = await response.json()
+        setFeedback(data);
+        setLoading(false);
+    }
+
 
     const editFeedback = (item) => {
         setFeedbackEdit({item, edit: true})
@@ -55,7 +50,8 @@ export const FeedbackProvider = ({children}) => {
         setReverse,
         editFeedback,
         feedbackEdit,
-        updateFeedback
+        updateFeedback,
+        loading
     }}>
         {children}
     </FeedbackContext.Provider>
