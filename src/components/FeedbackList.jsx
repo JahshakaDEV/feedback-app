4bcd669f0e7c516fe17d3adc7c8bd7c0bec4f846
Jannik Oeschger <oeschgerjannik@gmail.com>
2021-12-18 16:12:40 +0100

    💄 Added loading spinner
    Added spinner that is visible while the page is loading the feedback

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,17 +2,18 @@ import React, {useContext} from 'react';
 import FeedbackItem from "./FeedbackItem";
 import {AnimatePresence, motion} from "framer-motion";
 import FeedbackContext from "../context/FeedbackContext";
+import Spinner from "./shared/Spinner";
 
 function FeedbackList() {
 
-    const {feedback, reverse} = useContext(FeedbackContext)
+    const {feedback, reverse, loading} = useContext(FeedbackContext)
 
-    if (!feedback || feedback.length === 0) {
+    if (!loading && (!feedback || feedback.length === 0)) {
         return <p>No Feedback yet!</p>
     }
 
-    return (
-        <div className={"feedback-list"}>
+    return loading ? <Spinner/> :
+        (<div className={"feedback-list"}>
             <AnimatePresence>
                 {feedback.map((item) => (
                     <motion.div
@@ -29,8 +30,7 @@ function FeedbackList() {
                     </motion.div>
                 ))}
             </AnimatePresence>
-        </div>
-    );
+        </div>)
 }
 
 export default FeedbackList;
